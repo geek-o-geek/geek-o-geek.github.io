fcb1b866f666315f599b54fcb57ee46b98d5a06b
geekogeek <arjun.sisodia8@gmail.com>
2020-12-03 15:06:10 +0530

    bug fix

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+var initialColor = document.getElementById("header").style.color;
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
+    document.getElementById("header").style.color = "#fff";
+    document.getElementById("menu").style.opacity = "0.7";
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("header").style.color = initialColor;
+    document.getElementById("menu").style.opacity = "1";
   }
 }
